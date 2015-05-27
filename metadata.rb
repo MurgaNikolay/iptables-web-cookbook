ce0dffb1a0e797d0445cd1e264f90f3799656803
Nikolay Murga <nikolay.m@murga.kiev.ua>
2015-05-27 13:38:55 +0300

    Try to recurcive

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nikolay.m@randrmusic.com'
 license          'All rights reserved'
 description      'Installs/Configures iptables-web-cookbook'
 long_description 'Installs/Configures iptables-web-cookbook'
-version          '0.4.0'
+version          '0.4.1'
 
 
 depends          'mysql'
