7991c4e569d748f169ee4068e9005aaac2d6095a
Nikolay Murga <nikolay.m@murga.kiev.ua>
2015-07-10 17:04:59 +0300

    Change static_rules format in default attrs

--- attributes/client.rb
+++ attributes/client.rb
@@ -8,7 +8,7 @@ default['iptables_web']['client']['custer_name'] = 'production'
 default['iptables_web']['client']['install_method'] = 'rvm'
 default['iptables_web']['client']['rvm']['ruby'] = '2.0.0'
 default['iptables_web']['client']['rvm']['gemset'] = 'iptables_web'
-default['iptables_web']['client']['static_rules'] = [
+default['iptables_web']['client']['static_rules']['filter'] = [
   '-A INPUT -i lo -j ACCEPT',
   '-A FORWARD -i lo -j ACCEPT',
   '-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT',
